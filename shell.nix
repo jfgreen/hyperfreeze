53bdb13fbb3125b3eec1a8d4b81e07c9fc927894
James Green <1288679+jfgreen@users.noreply.github.com>
2024-06-21 21:44:47 +0100

    Bump nix packages 23.11 -> 24.05

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
-# nixos-23.11
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/219951b495fc2eac67b1456824cc1ec1fd2ee659.tar.gz") {} }:
+# nixos-24.05
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/752c634c09ceb50c45e751f8791cb45cb3d46c9e.tar.gz") {} }:
   pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     cargo
