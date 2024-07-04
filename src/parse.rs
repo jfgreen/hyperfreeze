72f0469ee4fdfd73ba8e9b0e5616d433dc9bedcf
James Green <1288679+jfgreen@users.noreply.github.com>
2024-07-04 20:48:35 +0100

    Parse strike through words

--- src/parse.rs
+++ src/parse.rs
@@ -5,6 +5,15 @@ struct Document<'a> {
     nodes: Box<[Node<'a>]>,
 }
 
+// We create a document tree, not a token stream, so that:
+// - Its easy to generate a table of contents
+// - Links dont have to be defined up front
+// - The types model the overall semantic structure of a doc
+// - It is clear as to which combinations of nodes can be nested
+//
+// If we didnt do these here, then HTML generation would have to build
+// its own tree, or make more than one pass of the token stream.
+
 //TODO: Is there a more efficent way of representing a document tree?
 // Think: Flatter, all pre-allocated, contigious
 #[derive(PartialEq, Eq, Debug)]
@@ -12,6 +21,7 @@ enum Node<'a> {
     Word(&'a str),
     EmphasisedWords(Box<[&'a str]>),
     BoldWords(Box<[&'a str]>),
+    StrikethroughWords(Box<[&'a str]>),
 }
 
 #[derive(Debug)]
@@ -29,10 +39,12 @@ fn parse(input: &str) -> Result<Document, ParseError> {
     while let Some(token) = tokens.next() {
         //TODO: Would lookahead be useful?
 
+        //FIXME: This is getting a bit unwieldy
         match token {
             Token::Word(word) => nodes.push(Node::Word(word)),
             Token::EmphasisDelimiter => nodes.push(parse_emphasised_text(&mut tokens)),
             Token::BoldDelimiter => nodes.push(parse_bold_text(&mut tokens)),
+            Token::StrikethroughDelimiter => nodes.push(parse_strikethrough_text(&mut tokens)),
             Token::Whitespace => (),
             _ => panic!("unexpected token"), // TODO: Propper error handling
         }
@@ -43,6 +55,7 @@ fn parse(input: &str) -> Result<Document, ParseError> {
     })
 }
 
+//FIXME: This is a bit repetative, generic "parse styled text?"
 fn parse_emphasised_text<'a, 'b>(tokens: &'a mut Tokeniser<'b>) -> Node<'b> {
     let mut words: Vec<&'b str> = Vec::new();
 
@@ -58,7 +71,6 @@ fn parse_emphasised_text<'a, 'b>(tokens: &'a mut Tokeniser<'b>) -> Node<'b> {
     Node::EmphasisedWords(words.into_boxed_slice())
 }
 
-//FIXME: This is a bit repetative
 fn parse_bold_text<'a, 'b>(tokens: &'a mut Tokeniser<'b>) -> Node<'b> {
     let mut words: Vec<&'b str> = Vec::new();
 
@@ -72,6 +84,21 @@ fn parse_bold_text<'a, 'b>(tokens: &'a mut Tokeniser<'b>) -> Node<'b> {
     }
 
     Node::BoldWords(words.into_boxed_slice())
+}
+
+fn parse_strikethrough_text<'a, 'b>(tokens: &'a mut Tokeniser<'b>) -> Node<'b> {
+    let mut words: Vec<&'b str> = Vec::new();
+
+    loop {
+        match tokens.next() {
+            Some(Token::Word(word)) => words.push(word),
+            Some(Token::Whitespace) => (),
+            Some(Token::StrikethroughDelimiter) => break,
+            _ => panic!("unexpected token"), // TODO: Propper error handling
+        }
+    }
+
+    Node::StrikethroughWords(words.into_boxed_slice())
 }
 
 #[cfg(test)]
@@ -132,6 +159,27 @@ mod test {
         assert_eq!(actual, expected);
     }
 
+    #[test]
+    fn strikethrough_words() {
+        let input = "Cats are -ok i guess- magnificant";
+
+        let expected = Document {
+            nodes: Box::new([
+                Node::Word("Cats"),
+                Node::Word("are"),
+                Node::StrikethroughWords(Box::new(["ok", "i", "guess"])),
+                Node::Word("magnificant"),
+            ]),
+        };
+
+        let actual = parse(input).unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
     //TODO: What do we do about 'Things like_this example'?
-    //TODO: ALso:  "Rules cats must follow: __"
+    //TODO: Also: "Rules cats must follow: __"
+    //TODO: Also: "Or-this-example"
+    //TODO: Also: "or this-"
+    //TODO: Also: "-or this"
 }
