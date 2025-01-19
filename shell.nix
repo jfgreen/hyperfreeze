c876a2fc74b055ad4d61e0072d5e16b915fdf250
James Green <1288679+jfgreen@users.noreply.github.com>
2025-01-19 22:05:48 +0000

    Update nix shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
-# nixos-24.05
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/752c634c09ceb50c45e751f8791cb45cb3d46c9e.tar.gz") {} }:
+# nixos-24.11
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3f0a8ac25fb674611b98089ca3a5dd6480175751.tar.gz") {} }:
   pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     cargo
