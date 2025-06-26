acbb32eaeb029dc66e1dd2e88c1f330d28822f55
James Green <1288679+jfgreen@users.noreply.github.com>
2025-06-26 20:45:49 +0100

    Use nixpkgs in shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,4 @@
-# nixos-24.11
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3f0a8ac25fb674611b98089ca3a5dd6480175751.tar.gz") {} }:
+{ pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     cargo
